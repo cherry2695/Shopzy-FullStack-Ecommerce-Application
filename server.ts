5e1f0e7d92cb565f126247024515ee14f69bd744
cherry2695 <chanikya955@gmail.com>
2026-06-09 10:59:50 +0530

    Added project screenshots to README

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = 5000;
 
   // 1. Establish Database Connection (Mongoose with JSON backup engines)
   await initDb();
